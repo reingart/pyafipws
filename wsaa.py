06c901de5e5060b93cee5ce9cec05e94f0003ed7
Mariano Reingart <reingart@gmail.com>
2011-03-04 15:40:32 -0300

    WSAA: ajusto licencia a GPLv3 para la nueva version interna 2.0

--- wsaa.py
+++ wsaa.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python
 # -*- coding: latin-1 -*-
 # This program is free software; you can redistribute it and/or modify
-# it under the terms of the GNU Lesser General Public License as published by the
+# it under the terms of the GNU General Public License as published by the
 # Free Software Foundation; either version 3, or (at your option) any later
 # version.
 #
