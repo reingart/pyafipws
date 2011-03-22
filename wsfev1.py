e46726c390fbbeed980923ec0d601121731c866e
Mariano Reingart <reingart@gmail.com>
2011-03-22 04:56:24 -0300

    WSFEv1: agrego rece.ini en instalador

--- wsfev1.py
+++ wsfev1.py
@@ -28,7 +28,7 @@ import traceback
 from cStringIO import StringIO
 from pysimplesoap.client import SimpleXMLElement, SoapClient, SoapFault, parse_proxy, set_http_wrapper
 
-HOMO = True
+HOMO = False
 
 #WSDL="https://www.sistemasagiles.com.ar/simulador/wsfev1/call/soap?WSDL=None"
 WSDL="https://wswhomo.afip.gov.ar/wsfev1/service.asmx?WSDL"
