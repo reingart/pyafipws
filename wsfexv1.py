9a8feb6928785c3810c999a66dafbb368bce9385
Mariano Reingart <reingart@gmail.com>
2011-07-06 10:42:17 -0300

    ajusto version WSFEXv1

--- wsfexv1.py
+++ wsfexv1.py
@@ -17,7 +17,7 @@ electrónico del web service WSFEXv1 de AFIP (Factura Electrónica Exportación V1)
 __author__ = "Mariano Reingart (reingart@gmail.com)"
 __copyright__ = "Copyright (C) 2011 Mariano Reingart"
 __license__ = "GPL 3.0"
-__version__ = "1.00a"
+__version__ = "1.01a"
 
 import datetime
 import decimal
