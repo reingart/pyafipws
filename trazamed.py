557ac47a5aa8578bf1aef7533df74b2b16bd32a2
Mariano Reingart <reingart@gmail.com>
2011-12-07 01:40:18 -0300

    TrazaMed: actualizo nro version

--- trazamed.py
+++ trazamed.py
@@ -15,7 +15,7 @@
 __author__ = "Mariano Reingart <reingart@gmail.com>"
 __copyright__ = "Copyright (C) 2011 Mariano Reingart"
 __license__ = "GPL 3.0"
-__version__ = "1.01b"
+__version__ = "1.02a"
 
 import os
 import socket
