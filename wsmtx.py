ea885c431ec65df23634d6f0d57adf9637943fd5
Mariano Reingart <reingart@gmail.com>
2012-09-02 13:14:03 -0300

    WSMTX: ajusto metodo para listar puntos de venta de cae no informados

--- wsmtx.py
+++ wsmtx.py
@@ -17,7 +17,7 @@ WSMTX de AFIP (Factura Electrónica Mercado Interno RG2904 opción A con detalle)
 __author__ = "Mariano Reingart <reingart@gmail.com>"
 __copyright__ = "Copyright (C) 2010 Mariano Reingart"
 __license__ = "GPL 3.0"
-__version__ = "1.07b"
+__version__ = "1.07d"
 
 import datetime
 import decimal
@@ -724,7 +724,7 @@ class WSMTXCA:
             authRequest={'token': self.Token, 'sign': self.Sign, 'cuitRepresentada': self.Cuit},
             CAEA=caea,
             )
-        return ["%(numeroPuntoVenta)s: bloqueado=%(bloqueado)s baja=%(fechaBaja)s" % p
+        return [" ".join([("%s=%s" % (k, v)) for k, v in p['puntoVenta'].items()])
                  for p in ret['arrayPuntosVenta']]
 
     @property
