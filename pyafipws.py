ab35f54290601e58aaf66bc1dac836dd1413b932
Mariano Reingart <reingart@gmail.com>
2013-05-31 13:54:48 -0300

    adjuste version 1.31a

--- pyafipws.py
+++ pyafipws.py
@@ -15,7 +15,7 @@
 __author__ = "Mariano Reingart (reingart@gmail.com)"
 __copyright__ = "Copyright (C) 2008 Mariano Reingart"
 __license__ = "GPL 3.0"
-__version__ = "1.30a"
+__version__ = "1.31a"
 
 import sys
 import wsfe, wsbfe, wsfex, wsctg, wdigdepfiel
