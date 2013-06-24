9a88e433e94c81f52a58621d6ce1d526124533e2
Mariano Reingart <reingart@gmail.com>
2013-06-24 01:22:03 -0300

    LibPyAFipWs: agrego ejemplo en C (basico, WSAA preliminar)

---
+++ ejemplos/factura_electronica.c
@@ -0,0 +1,30 @@
+/* 
+ * Ejemplo de Uso de Biblioteca LibPyAfipWs (.DLL / .so)
+ * con Web Service Autenticación / Factura Electrónica AFIP
+ * 2013 (C) Mariano Reingart <reingart@gmail.com>
+ * Licencia: GPLv3
+ * Requerimientos: scripts wsaa.py y libpyafipws.h / libpyafipws.c
+ * Nota: debe configurar certificado, clave privada y CUIT en rece.ini
+ * Documentacion: 
+ *  http://www.sistemasagiles.com.ar/trac/wiki/LibPyAfipWs
+ *  http://www.sistemasagiles.com.ar/trac/wiki/ManualPyAfipWs
+ */
+
+#include "libpyafipws.h"
+
+int main(int argc, char *argv[]) {
+  char *tra, *cms, *ta;
+  /* prueba generica */
+  test();
+  /* Generar ticket de requerimiento de acceso */
+  tra = WSAA_CreateTRA("wsfe", 999);
+  printf("TRA:\n%s\n", tra);
+  /* Firmar criptograficamente el mensaje */
+  cms = WSAA_SignTRA(tra, "reingart.crt", "reingart.key");
+  printf("CMS:\n%s\n", cms);  
+  /* Llamar al webservice y obtener el ticket de acceso */
+  ta = WSAA_LoginCMS(cms);
+  printf("TA:\n%s\n", ta);  
+  return 0;
+}
+
