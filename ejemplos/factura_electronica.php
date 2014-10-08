0a3aee14a8b300d74fa97eea9efc7860181b2ba6
Mariano Reingart <reingart@gmail.com>
2014-10-08 14:49:25 -0300

    Ajusto ejemplo PHP:
     * Datos de prueba correctos (fechas, moneda, etc.) para obtener CAE
     * Obtener ultimo numero de factura electronica emitida
     * Temas menores (rutas, mensajes de error, etc)

--- ejemplos/factura_electronica.php
+++ ejemplos/factura_electronica.php
@@ -6,31 +6,33 @@
 // Requerimientos: scripts rece1.py (CAE) y pyfepdf.py (generación de PDF)
 // Nota: debe configurar certificado, clave privada y CUIT en rece.ini
 // Documentacion: http://www.sistemasagiles.com.ar/trac/wiki/ManualPyAfipWs
+// Importante: configurar en rece.ini entrada=factura.json y salida=salida.json
+// Nota: cambiar ruta a rece1.py / pyfepdf.py y rece.ini (de corresponder)
 
 // Establezco los valores de la factura a autorizar:
 $factura = array(
      'id' => 0,                     // identificador único (obligatorio WSFEX)
 
      'punto_vta' => 4000,
-     'tipo_cbte' => 2,              // 1: FCA, 2: NDA, 3:NCA, 6: FCB, 11: FCC
-     'cbte_nro' => 12345678,
+     'tipo_cbte' => 1,              // 1: FCA, 2: NDA, 3:NCA, 6: FCB, 11: FCC
+     'cbte_nro' => 0,               // solicitar proximo con /ult
 
      'tipo_doc' => 80,              // 96: DNI, 80: CUIT, 99: Consumidor Final
      'nro_doc' => '30000000007',    // Nro. de CUIT o DNI
 
-     'fecha_cbte' => '20130605',
-     'fecha_serv_desde' => '20130605',
-     'fecha_serv_hasta' => '20130605',
-     'fecha_venc_pago' => '20130605',
+     'fecha_cbte' => date('Ymd'),   // Formato AAAAMMDD
+     'fecha_serv_desde' => NULL,    // competar si concepto > 1
+     'fecha_serv_hasta' => NULL,    // competar si concepto > 1
+     'fecha_venc_pago' => NULL,     // competar si concepto > 1
 
-     'concepto' => 3,               // 1: Productos, 2: Servicios, 3/4: Ambos
+     'concepto' => 1,               // 1: Productos, 2: Servicios, 3/4: Ambos
 
      'nombre_cliente' => 'Joao Da Silva',
      'domicilio_cliente' => 'Rua 76 km 34.5 Alagoas',
      'pais_dst_cmp' => 16,  // solo exportacion
 
-     'moneda_ctz' => 0.5,   // 1 para pesos
-     'moneda_id' => '012',  // 'PES' para pesos
+     'moneda_ctz' => 1,   // 1 para pesos
+     'moneda_id' => 'PES',  // 'PES': pesos, 'DOL': dolares (solo exportacion)
 
      'obs_comerciales' => 'Observaciones Comerciales, texto libre',
      'obs_generales' => 'Observaciones Generales, texto libre',
@@ -44,14 +46,14 @@ $factura = array(
      'imp_tot_conc' => '3.00',          // no gravado
      'imp_iva' => '21.00',              // IVA liquidado
      'imp_trib' => '1.00',              // otros tributos
-     'imp_total' => '122.00',           // total de la factura
+     'imp_total' => '127.00',           // total de la factura
 
-     // Datos devueltos por AFIP (completados a modo de ejemplo):
-     'cae' => '61123022925855',
-     'fecha_vto' => '20110320',
-     'motivos_obs' => '11',
-     'err_code' => 'OK',
-     
+     // Datos devueltos por AFIP (completados luego al llamar al webservice):
+     'cae' => '',                       // ej. '61123022925855'
+     'fecha_vto' => '',                 // ej. '20110320'
+     'motivos_obs' => '',               // ej. '11'
+     'err_code' => '',                  // ej. 'OK'
+
      'descuento' => 0,
      'detalles' => array (
           array(
@@ -78,10 +80,11 @@ $factura = array(
              'iva_id' => 5,
           ),
         ),
-     'cbtes_asoc' => array (
-        array('cbte_nro' => 1234, 'cbte_punto_vta' => 2, 'cbte_tipo' => 91, ),
-        array('cbte_nro' => 1234, 'cbte_punto_vta' => 2, 'cbte_tipo' => 5, ),
-      ),
+     // Comprobantes asociados (solo notas de crédito y débito):
+     //'cbtes_asoc' => array (
+     //   array('cbte_nro' => 1234, 'cbte_punto_vta' => 2, 'cbte_tipo' => 91, ),
+     //   array('cbte_nro' => 1234, 'cbte_punto_vta' => 2, 'cbte_tipo' => 5, ),
+     // ),
      'tributos' => array (
           array(
              'alic' => '1.00',
@@ -96,21 +99,47 @@ $factura = array(
 );
 
 
-// Ejemplo para guardar el archivo json:
+// Guardar el archivo json para consultar la ultimo numero de factura:
+$json = file_put_contents('./factura.json', json_encode(array($factura)));
+
+// Obtener el último número para este tipo de comprobante / punto de venta:
+exec("python ./rece1.py rece.ini /json /ult 1 4000");
+
+$json = file_get_contents('./salida.json');
+$facturas = json_decode($json, True);
+
+// leo el ultimo numero de factura del archivo procesado (salida)
+$cbte_nro = intval($facturas[0]['cbt_desde']) + 1;
+echo "Proximo Numero: ", $cbte_nro, "\n\r";
+
+// Vuelvo a guardar el archivo json para actualizar el número de factura:
+$factura['cbt_desde'] = $cbte_nro;  // para WSFEv1
+$factura['cbt_hasta'] = $cbte_nro;  // para WSFEv1
+$factura['cbte_nro'] = $cbte_nro;   // para PDF
 $json = file_put_contents('./factura.json', json_encode(array($factura)));
 
 // Obtención de CAE: llamo a la herramienta para WSFEv1
-exec("python ./rece1.py /json");
+exec("python ./rece1.py rece.ini /json");
 
 // Ejemplo para levantar el archivo json con el CAE obtenido:
-$json = file_get_contents('./factura.json');
+$json = file_get_contents('./salida.json');
 $facturas = json_decode($json, True);
 
 // leo el CAE del archivo procesado
 echo "CAE OBTENIDO: ", $facturas[0]['cae'], "\n\r";
+echo "Observaciones: ", $facturas[0]['motivos_obs'], "\n\r";
+echo "Errores: ", $facturas[0]['err_msg'], "\n\r";
 
-// Genero la factura en PDF
-exec("python ./pyfepdf.py --cargar --mostrar --json")
+// Vuelvo a guardar el archivo json para actualizar el CAE y otros datos:
+$factura['cae'] = $facturas[0]['cae'];
+$factura['fecha_vto'] = $facturas[0]['fch_venc_cae'];
+$factura['motivos_obs'] = $facturas[0]['motivos_obs'];
+$factura['err_code'] = $facturas[0]['err_code'];
+$factura['err_msg'] = $facturas[0]['err_msg'];
+$json = file_put_contents('./factura.json', json_encode(array($factura)));
+
+// Genero la factura en PDF (agregar --mostrar si se tiene visor de PDF)
+exec("python ./pyfepdf.py rece.ini --cargar --json")
 
 // leer factura.pdf o similar para obtener el documento generado
 
