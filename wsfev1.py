c072a0a2d337372ac0d61d5dd2830195ef7bb9f1
Mariano Reingart <reingart@gmail.com>
2016-04-06 00:24:00 -0300

    WSFEv1: ajuste eventos (informativos)

--- wsfev1.py
+++ wsfev1.py
@@ -21,7 +21,7 @@ Más info: http://www.sistemasagiles.com.ar/trac/wiki/ProyectoWSFEv1
 __author__ = "Mariano Reingart <reingart@gmail.com>"
 __copyright__ = "Copyright (C) 2010-2015 Mariano Reingart"
 __license__ = "GPL 3.0"
-__version__ = "1.17b"
+__version__ = "1.17c"
 
 import datetime
 import decimal
@@ -108,7 +108,7 @@ class WSFEv1(BaseWS):
             self.ErrMsg = '\n'.join(self.Errores)
         if 'Events' in ret:
             events = ret['Events']
-            self.Eventos = ['%s: %s' % (evt['code'], evt['msg']) for evt in events]
+            self.Eventos = ['%s: %s' % (evt['Evt']['Code'], evt['Evt']['Msg']) for evt in events]
 
     @inicializar_y_capturar_excepciones
     def Dummy(self):
