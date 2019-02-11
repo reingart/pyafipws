d2be911c57c767439771ebc7186980c25fae8fd6
Mariano Reingart <reingart@gmail.com>
2019-02-11 01:58:23 -0300

    PyFEPDF: revert autopep8 W601 fix (caused an infinite loop...)
    NOTE: template is not a dictionary, has_key is a real method.

--- pyfepdf.py
+++ pyfepdf.py
@@ -517,13 +517,13 @@ class FEPDF:
                     fact[k] = ds.replace('<br/>', '\n')
 
             # divido las observaciones por linea:
-            if fact.get('obs_generales') and 'obs' not in f and 'ObservacionesGenerales1' not in f:
+            if fact.get('obs_generales') and not f.has_key('obs') and not f.has_key('ObservacionesGenerales1'):
                 obs = "\n<U>Observaciones:</U>\n\n" + fact['obs_generales']
                 # limpiar texto (campos dbf) y reemplazar saltos de linea:
                 obs = obs.replace('\x00', '').replace('<br/>', '\n')
                 for ds in f.split_multicell(obs, 'Item.Descripcion01'):
                     li_items.append(dict(codigo=None, ds=ds, qty=None, umed=None, precio=None, importe=None))
-            if fact.get('obs_comerciales') and 'obs_comerciales' not in f and 'ObservacionesComerciales1' not in f:
+            if fact.get('obs_comerciales') and not f.has_key('obs_comerciales') and not f.has_key('ObservacionesComerciales1'):
                 obs = "\n<U>Observaciones Comerciales:</U>\n\n" + fact['obs_comerciales']
                 # limpiar texto (campos dbf) y reemplazar saltos de linea:
                 obs = obs.replace('\x00', '').replace('<br/>', '\n')
@@ -534,13 +534,13 @@ class FEPDF:
             permisos = ['Codigo de Despacho %s - Destino de la mercadería: %s' % (
                 p['id_permiso'], self.paises.get(p['dst_merc'], p['dst_merc']))
                 for p in fact.get('permisos', [])]
-            #import dbg; dbg.set_trace()
-            if 'permiso.id1' in f and "permiso.delivery1" in f:
+
+            if f.has_key('permiso.id1') and f.has_key("permiso.delivery1"):
                 for i, p in enumerate(fact.get('permisos', [])):
                     self.AgregarDato("permiso.id%d" % (i + 1), p['id_permiso'])
                     pais_dst = self.paises.get(p['dst_merc'], p['dst_merc'])
                     self.AgregarDato("permiso.delivery%d" % (i + 1), pais_dst)
-            elif 'permisos' not in f and permisos:
+            elif not f.has_key('permisos') and permisos:
                 obs = "\n<U>Permisos de Embarque:</U>\n\n" + '\n'.join(permisos)
                 for ds in f.split_multicell(obs, 'Item.Descripcion01'):
                     li_items.append(dict(codigo=None, ds=ds, qty=None, umed=None, precio=None, importe=None))
@@ -549,7 +549,7 @@ class FEPDF:
             # agrego comprobantes asociados
             cmps_asoc = ['%s %s %s' % self.fmt_fact(c['cbte_tipo'], c['cbte_punto_vta'], c['cbte_nro'])
                          for c in fact.get('cbtes_asoc', [])]
-            if 'cmps_asoc' not in f and cmps_asoc:
+            if not f.has_key('cmps_asoc') and cmps_asoc:
                 obs = "\n<U>Comprobantes Asociados:</U>\n\n" + '\n'.join(cmps_asoc)
                 for ds in f.split_multicell(obs, 'Item.Descripcion01'):
                     li_items.append(dict(codigo=None, ds=ds, qty=None, umed=None, precio=None, importe=None))
@@ -572,7 +572,7 @@ class FEPDF:
             # mostrar las validaciones no excluyentes de AFIP (observaciones)
 
             if fact.get('motivos_obs') and fact['motivos_obs'] != '00':
-                if 'motivos_ds.L' not in f:
+                if not f.has_key('motivos_ds.L'):
                     motivos_ds = "Irregularidades observadas por AFIP (F136): %s" % fact['motivos_obs']
                 else:
                     motivos_ds = "%s" % fact['motivos_obs']
@@ -583,7 +583,7 @@ class FEPDF:
 
             if letra_fact in ('A', 'M'):
                 msg_no_iva = "\nEl IVA discriminado no puede computarse como Crédito Fiscal (RG2485/08 Art. 30 inc. c)."
-                if 'leyenda_credito_fiscal' not in f and motivos_ds:
+                if not f.has_key('leyenda_credito_fiscal') and motivos_ds:
                     motivos_ds += msg_no_iva
 
             copias = {1: 'Original', 2: 'Duplicado', 3: 'Triplicado'}
@@ -687,7 +687,7 @@ class FEPDF:
                             if it['codigo'] is not None:
                                 f.set('Item.Codigo%02d' % li, it['codigo'])
                             if it['umed'] is not None:
-                                if it['umed'] and "Item.Umed_ds01" in f:
+                                if it['umed'] and f.has_key("Item.Umed_ds01"):
                                     # recortar descripción:
                                     umed_ds = self.umeds_ds.get(int(it['umed']))
                                     s = f.split_multicell(umed_ds, 'Item.Umed_ds01')
@@ -829,9 +829,9 @@ class FEPDF:
 
                     # Datos del pie de factura (obtenidos desde AFIP):
                     f.set('motivos_ds', motivos_ds)
-                    if 'motivos_ds1' in f and motivos_ds:
+                    if f.has_key('motivos_ds1') and motivos_ds:
                         if letra_fact in ('A', 'M'):
-                            if 'leyenda_credito_fiscal' in f:
+                            if f.has_key('leyenda_credito_fiscal'):
                                 f.set('leyenda_credito_fiscal', msg_no_iva)
                         for i, txt in enumerate(f.split_multicell(motivos_ds, 'motivos_ds1')):
                             f.set('motivos_ds%d' % (i + 1), txt)
@@ -863,13 +863,13 @@ class FEPDF:
                         f.set('estado', "")  # compatibilidad hacia atras
 
                     # colocar campos de observaciones (si no van en ds)
-                    if 'observacionesgenerales1' in f and 'obs_generales' in fact:
+                    if f.has_key('observacionesgenerales1') and 'obs_generales' in fact:
                         for i, txt in enumerate(f.split_multicell(fact['obs_generales'], 'ObservacionesGenerales1')):
                             f.set('ObservacionesGenerales%d' % (i + 1), txt)
-                    if 'observacionescomerciales1' in f and 'obs_comerciales' in fact:
+                    if f.has_key('observacionescomerciales1') and 'obs_comerciales' in fact:
                         for i, txt in enumerate(f.split_multicell(fact['obs_comerciales'], 'ObservacionesComerciales1')):
                             f.set('ObservacionesComerciales%d' % (i + 1), txt)
-                    if 'enletras1' in f and 'en_letras' in fact:
+                    if f.has_key('enletras1') and 'en_letras' in fact:
                         for i, txt in enumerate(f.split_multicell(fact['en_letras'], 'EnLetras1')):
                             f.set('EnLetras%d' % (i + 1), txt)
 
