a88a53b481300886d404ad5002a5a530063cbf44
Mariano Reingart <reingart@gmail.com>
2021-02-21 19:15:29 -0300

    Revert "Actualizado el metodo para guardar datos en el padron"
    This reverts commit 7c71372eb384f6255f6f3824e8c73a0b3509d0d2.

--- padron.py
+++ padron.py
@@ -297,26 +297,16 @@ class PadronAFIP():
         return [dict(fila) for fila in filas]
 
     @inicializar_y_capturar_excepciones_simple
-    def Guardar(self, tipo_doc, nro_doc, denominacion, cat_iva, direccion, 
-               email, imp_ganancias='NI', imp_iva='NI', monotributo='NI', 
-               integrante_soc='N', empleador='N'):
+    def Guardar(self, tipo_doc, nro_doc, denominacion, cat_iva, direccion, email):
         "Agregar o actualizar los datos del cliente"
         if self.Buscar(nro_doc, tipo_doc):
-            sql = ("UPDATE padron SET denominacion=?, cat_iva=?, email=?, "
-                    "imp_ganancias=?, imp_iva=?, monotributo=?, "
-                    "integrante_soc=?, empleador=? "
+            sql = ("UPDATE padron SET denominacion=?, cat_iva=?, email=? "
                     "WHERE tipo_doc=? AND nro_doc=?")
-            params = [denominacion, cat_iva, email, imp_ganancias, 
-                      imp_iva, monotributo, integrante_soc, empleador,
-                     tipo_doc, nro_doc]
+            params = [denominacion, cat_iva, email, tipo_doc, nro_doc]
         else:
             sql = ("INSERT INTO padron (tipo_doc, nro_doc, denominacion, "
-                    "cat_iva, email, imp_ganancias, imp_iva, monotributo, "
-                    "integrante_soc, empleador) "
-                    "VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)")
-            params = [tipo_doc, nro_doc, denominacion, cat_iva, email,
-                      imp_ganancias, imp_iva, monotributo, 
-                      integrante_soc, empleador]
+                    "cat_iva, email) VALUES (?, ?, ?, ?, ?)")
+            params = [tipo_doc, nro_doc, denominacion, cat_iva, email]
         self.cursor.execute(sql, params)
         # agregar el domicilio solo si no existe:
         if direccion:
