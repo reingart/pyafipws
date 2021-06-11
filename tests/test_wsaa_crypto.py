d6e6182551d912abc0eff07a55f1744300f050b5
UTKARSH KUMAR <Utkarshdhsbgp@gmail.com>
2021-06-11 19:07:15 +0530

    added more unit tests for wsaa.py
    Signed-off-by: UTKARSH KUMAR <Utkarshdhsbgp@gmail.com>

--- tests/test_wsaa_crypto.py
+++ tests/test_wsaa_crypto.py
@@ -4,8 +4,17 @@ from past.builtins import basestring
 
 from pyafipws.wsaa import WSAA
 
+import pytest
 
-def test_wsfev1_create_tra():
+
+@pytest.fixture
+def key_and_cert():
+    KEY = "reingart.key"
+    CERT = "reingart.crt"
+    return [KEY, CERT]
+
+
+def test_wsaa_create_tra():
     wsaa = WSAA()
     tra = wsaa.CreateTRA(service="wsfe")
     # TODO: return string
@@ -13,15 +22,15 @@ def test_wsfev1_create_tra():
     # sanity checks:
     assert isinstance(tra, basestring)
     assert tra.startswith(
-        '<?xml version="1.0" encoding="UTF-8"?>'
-        '<loginTicketRequest version="1.0">'
-        )
-    assert '<uniqueId>' in tra
-    assert '<expirationTime>' in tra
-    assert tra.endswith('<service>wsfe</service></loginTicketRequest>')
+        '<?xml version="1.0" encoding="UTF-8"?>' '<loginTicketRequest version="1.0">'
+    )
+    assert "<uniqueId>" in tra
+    assert "<expirationTime>" in tra
+    assert "<generationTime>" in tra
+    assert tra.endswith("<service>wsfe</service></loginTicketRequest>")
 
 
-def test_wsfev1_sign():
+def test_wsaa_sign():
     wsaa = WSAA()
     tra = '<?xml version="1.0" encoding="UTF-8"?><loginTicketRequest version="1.0"/>'
     # TODO: use certificate and private key as fixture / PEM text (not files)
@@ -33,3 +42,30 @@ def test_wsfev1_sign():
     assert isinstance(cms, str)
     out = base64.b64decode(cms)
     assert tra.encode("utf8") in out
+
+
+def test_wsaa_sign_tra(key_and_cert):
+    wsaa = WSAA()
+
+    tra = wsaa.CreateTRA("wsfe")
+    sign = wsaa.SignTRA(tra, key_and_cert[1], key_and_cert[0])
+
+    assert isinstance(sign, str)
+    assert sign.startswith("MIIG+")
+
+
+def test_wsaa_sign_tra_inline(key_and_cert):
+    wsaa = WSAA()
+
+    tra = wsaa.CreateTRA("wsfe")
+    sign = wsaa.SignTRA(tra, key_and_cert[1], key_and_cert[0])
+
+    sign_2 = wsaa.SignTRA(
+        tra, open(key_and_cert[1]).read(), open(key_and_cert[0]).read()
+    )
+
+    assert isinstance(sign, str)
+    assert sign.startswith("MIIG+")
+
+    assert isinstance(sign_2, str)
+    assert sign_2.startswith("MIIG+")
