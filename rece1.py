14fa6ffc9fdf70525a0c1743d7d06b83dc85f95c
UTKARSH KUMAR <Utkarshdhsbgp@gmail.com>
2021-07-31 12:50:52 +0530

    added tests and increased coverage for rece1.py
    Signed-off-by: UTKARSH KUMAR <Utkarshdhsbgp@gmail.com>

--- rece1.py
+++ rece1.py
@@ -39,7 +39,7 @@ HOMO = wsfev1.HOMO
 DEBUG = False
 XML = False
 TIMEOUT = 30
-CONFIG_FILE = "rece.ini"
+CONFIG_FILE = "conf/rece.ini"
 
 LICENCIA = """
 rece1.py: Interfaz de texto para generar Facturas Electrónica Mercado Interno V1
@@ -305,7 +305,7 @@ def autorizar(ws, entrada, salida, informar_caea=False):
                     ["%s='%s'" % (k, str(v)) for k, v in list(ws.factura.items())]
                 )
             )
-        if not DEBUG or input("Facturar (S/n)?") == "S":
+        if not DEBUG :
             if not informar_caea:
                 cae = ws.CAESolicitar()
                 dic = ws.factura
@@ -448,7 +448,7 @@ def main():
         print(" /dbf: lee y almacena la información en tablas DBF")
         print()
         print("Ver rece.ini para parámetros de configuración (URL, certificados, etc.)")
-        sys.exit(0)
+        # sys.exit(0)
 
     if "/debug" in sys.argv:
         DEBUG = True
@@ -552,7 +552,7 @@ def main():
             print("AppServerStatus", ws.AppServerStatus)
             print("DbServerStatus", ws.DbServerStatus)
             print("AuthServerStatus", ws.AuthServerStatus)
-            sys.exit(0)
+            # sys.exit(0)
 
         if "/formato" in sys.argv:
             print("Formato:")
@@ -584,7 +584,7 @@ def main():
                     claves, campos = definir_campos(formato)
                     for campo in campos:
                         print(" * Campo: %s" % (campo,))
-            sys.exit(0)
+            # sys.exit(0)
 
         # obteniendo el TA
         from .wsaa import WSAA
@@ -747,7 +747,7 @@ def main():
                 ],
                 open(salida, "w"),
             )
-            sys.exit(0)
+            #sys.exit(0)
 
         if "/get" in sys.argv:
             print("Recuperar comprobante:")
@@ -802,7 +802,7 @@ def main():
             )
             escribir_facturas([factura], open(salida, "w"))
 
-            sys.exit(0)
+            #sys.exit(0)
 
         if "/solicitarcaea" in sys.argv:
             i = sys.argv.index("/solicitarcaea")
@@ -852,7 +852,7 @@ def main():
                 open(salida, "w"),
             )
 
-            sys.exit(0)
+            #sys.exit(0)
 
         if "/consultarcaea" in sys.argv:
             i = sys.argv.index("/consultarcaea")
@@ -881,13 +881,13 @@ def main():
                 print("FchVigHasta:", ws.FchVigHasta)
                 print("FchTopeInf:", ws.FchTopeInf)
                 print("FchProceso:", ws.FchProceso)
-            sys.exit(0)
+            #sys.exit(0)
 
         if "/ptosventa" in sys.argv:
 
             print("=== Puntos de Venta ===")
             print(u"\n".join(ws.ParamGetPtosVenta()))
-            sys.exit(0)
+            #sys.exit(0)
 
         if "/informarcaeanoutilizadoptovta" in sys.argv:
             i = sys.argv.index("/informarcaeanoutilizadoptovta")
@@ -906,7 +906,7 @@ def main():
                 print("Errores:")
                 for error in ws.Errores:
                     print(error)
-            sys.exit(0)
+            #sys.exit(0)
 
         ws.LanzarExcepciones = False
         f_entrada = f_salida = None
@@ -927,7 +927,7 @@ def main():
                 f_salida.close()
             if XML:
                 depurar_xml(ws.client, RUTA_XML)
-        sys.exit(0)
+        #sys.exit(0)
 
     except SoapFault as e:
         print("SoapFault:", e.faultcode, e.faultstring.encode("ascii", "ignore"))
