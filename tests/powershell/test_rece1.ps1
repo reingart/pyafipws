47647b46a49b746ad22c696d5fd5513b8e221673
Mariano Reingart <reingart@gmail.com>
2022-11-20 14:38:41 -0300

    GHA: run RECE1 at toplevel for output

--- tests/powershell/test_rece1.ps1
+++ tests/powershell/test_rece1.ps1
@@ -1,11 +1,12 @@
+# call webservice to get last invoice number (Invoice type: 1 - Point of Sale: 2):
+RECE1.exe /ult 1 2 /json --debug --trace
+# convert the output file to json to read the results: 
+$json = Get-Content -Path salida.txt | ConvertFrom-Json
+
 describe TestUlt {
-    BeforeAll {
-        # call webservice to get last invoice number (Invoice type: 1 - Point of Sale: 2):
-        RECE1.exe /ult 1 2 /json --debug --trace
-        # convert the output file to json to read the results: 
-        $json = Get-Content -Path salida.txt | ConvertFrom-Json
+    Context {
+        it 'returns last invoice number' {
+            [int]$json[0].cbt_desde | Should -BeGreaterOrEqual 1
+        } 
     }
-    it 'returns last invoice number' {
-        [int]$json[0].cbt_desde | Should -BeGreaterOrEqual 1
-    } 
 }
