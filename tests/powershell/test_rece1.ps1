aef1ea820e58753d80ae4d2c010824462f361100
Mariano Reingart <reingart@gmail.com>
2022-11-20 16:16:51 -0300

    RECE1: group pester tests in context

--- tests/powershell/test_rece1.ps1
+++ tests/powershell/test_rece1.ps1
@@ -1,10 +1,11 @@
-# call webservice to get last invoice number (Invoice type: 1 - Point of Sale: 2):
-RECE1.exe /ult 1 2 /json --debug --trace
-# convert the output file to json to read the results: 
-$json = Get-Content -Path salida.txt | ConvertFrom-Json
-
 describe TestUlt {
-    Context {
+    Context "get last invoice - json interchange files" {
+        BeforeAll {
+            # call webservice to get last invoice number (Invoice type: 1 - Point of Sale: 2):
+            RECE1.exe /ult 1 2 /json --debug --trace
+            # convert the output file to json to read the results:
+            $json = Get-Content -Path salida.txt | ConvertFrom-Json
+        }
         it 'returns last invoice number' {
             [int]$json[0].cbt_desde | Should -BeGreaterOrEqual 1
         } 
