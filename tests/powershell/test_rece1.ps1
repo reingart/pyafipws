f81f195d923d6c7f269cc1d9d7cc7d549252d359
Mariano Reingart <reingart@gmail.com>
2022-11-20 13:11:54 -0300

    RECE1: powershell test (last invoice / json)

---
+++ tests/powershell/test_rece1.ps1
@@ -0,0 +1,10 @@
+describe TestUlt {
+    it 'returns last invoice number' {
+        # call webservice to get last invoice number (Invoice type: 1 - Point of Sale: 2):
+        RECE1.exe /ult 1 2 /json --debug --trace
+        # convert the output file to json to read the results: 
+        $json = Get-Content -Path salida.txt | ConvertFrom-Json
+        # check webservice result:
+        [int]$json[0].cbt_desde | Should -BeGreaterOrEqual 1
+    } 
+}
