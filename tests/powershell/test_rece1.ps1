ed28769083363d1aaa6d99f96ad0d20c42ca937b
Mariano Reingart <reingart@gmail.com>
2022-11-20 21:53:02 -0300

    RECE1: add ps test to authorize an invoice

--- tests/powershell/test_rece1.ps1
+++ tests/powershell/test_rece1.ps1
@@ -11,3 +11,16 @@ describe TestUlt {
         } 
     }
 }
+
+describe TestGetCAE {
+    Context "authorize invoice - json interchange files" {
+        BeforeAll {
+            # call webservice to get last invoice number (Invoice type: 1 - Point of Sale: 2):
+            RECE1.exe /prueba /json --debug --trace
+            # convert the output file to json to read the results:
+            $json = Get-Content -Path salida.txt | ConvertFrom-Json
+        }
+        it 'Invoice result ok' { $json[0].resultado | Should -Be 'A' }
+        it 'Invoice auth code ok' { $json[0].cae | Should -Not -BeNullOrEmpty }
+    }
+}
