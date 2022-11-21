e1c4166969171f42cdcaaba35b45dabcf6ed3680
Mariano Reingart <reingart@gmail.com>
2022-11-20 22:04:16 -0300

    RECE1: ps test invoice auth - text file (cobol)

--- tests/powershell/test_rece1.ps1
+++ tests/powershell/test_rece1.ps1
@@ -15,12 +15,22 @@ describe TestUlt {
 describe TestGetCAE {
     Context "authorize invoice - json interchange files" {
         BeforeAll {
-            # call webservice to get last invoice number (Invoice type: 1 - Point of Sale: 2):
+            # call webservice to get last invoice number:
             RECE1.exe /prueba /json --debug --trace
             # convert the output file to json to read the results:
             $json = Get-Content -Path salida.txt | ConvertFrom-Json
         }
         it 'Invoice result ok' { $json[0].resultado | Should -Be 'A' }
         it 'Invoice auth code ok' { $json[0].cae | Should -Not -BeNullOrEmpty }
+    }
+    Context "authorize invoice - fixed text fields (cobol) interchange files" {
+        BeforeAll {
+            # call webservice to get last invoice number:
+            RECE1.exe /prueba --debug --trace
+            # get the first line for invoice headers (see /formato):
+            $header = ((Get-Content -Path salida.txt) -split '\n')[0]
+        }
+        it 'Invoice result ok' { $header[193] | Should -Be 'A' }
+        it 'Invoice auth code ok' { [long]$header.substring(171, 14) | Should -BeGreaterOrEqual 1 }
     }
 }
