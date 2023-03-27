0262bfbce28e57411eee2eded213374c015151c1
HanslettTheDev <HanslettTheDev@gmail.com>
2023-03-27 15:14:33 +0100

    feat: get all required dependencies in setup.py from issue #105
    Signed-off-by: HanslettTheDev <HanslettTheDev@gmail.com>

--- get_dep.py
+++ get_dep.py
@@ -1,6 +1,7 @@
 import subprocess
 
 def get_dependecies() -> list:
+    # The subprocess.Popen is used for backward compatibility with python 2.7
     freeze = subprocess.Popen("pip freeze", shell=False, stdout=subprocess.PIPE, stderr=subprocess.PIPE).communicate()
     g = freeze[0].decode("utf-8").split("\r\n")
     dependencies = [">=".join(x.split("==")) for x in g]
