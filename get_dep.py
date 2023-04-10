bb768e5576f65ba7def7a6cec5d734391ff5b886
HanslettTheDev <HanslettTheDev@gmail.com>
2023-04-11 00:49:10 +0100

    fix: fixed the issue with pip not detecting links
    Signed-off-by: HanslettTheDev <HanslettTheDev@gmail.com>

--- get_dep.py
+++ get_dep.py
@@ -1,8 +1,24 @@
 import os
 
-def get_dependecies() -> list:
-    requirements_path = os.path.join(os.path.abspath(os.getcwd()), "requirements.txt")
+
+def get_dependecies():
+    blob = "git+https://github.com"
+    requirements_path = os.path.join(
+        os.path.abspath(os.getcwd()),
+        "requirements.txt"
+    )
     if os.path.isfile(requirements_path):
         with open(requirements_path) as f:
-            dependencies = [">=".join(x.split("==")) for x in f.read().splitlines()]
-    return dependencies
+            dependencies = [
+                ">=".join(x.split("==")) for x in f.read().splitlines()
+            ]
+            for x in dependencies:
+                if x.startswith(blob):
+                    # split the text and join them with the @ command
+                    # index 3 holds the name of the module
+                    chunks = x.split("/")
+                    dependencies[dependencies.index(x)] = x.replace(
+                        blob, chunks[3] + " @ " + chunks[0]
+                    )
+                    break
+        return dependencies
