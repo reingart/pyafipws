c395eab95e1dafe5380974e4b56ec62876b0af56
SONIABHISHEK121 <as630@snu.edu.in>
2024-08-07 17:05:10 +0530

    feat(tests): add PowerShell tests for factura_electronica.vbs and remito_electronico_carico.vbs
    - Add test_factura_electronica.ps1 to test AFIP electronic invoicing
    - Add test_remito_electronico_carnico.ps1 to test meat shipping vouchers
    - Update test_pyqr.ps1 to remove commented code
    
    Signed-off-by: SONIABHISHEK121 <as630@snu.edu.in>

--- tests/powershell/test_pyqr.ps1
+++ tests/powershell/test_pyqr.ps1
@@ -1,37 +1,6 @@
 # # Test script for PyQR COM object
 
 # # Create PyQR COM object
-# $PyQR = New-Object -ComObject PyQR
-
-# Describe "PyQR Tests" {
-#     It "Can create a file" {
-#         $archivo = $PyQR.CrearArchivo()
-#         $archivo | Should -Not -BeNullOrEmpty
-#     }
-
-#     It "Can generate QR image" {
-#         $ver = 1
-#         $fecha = "2020-10-13"
-#         $cuit = 30000000007
-#         $pto_vta = 10
-#         $tipo_cmp = 1
-#         $nro_cmp = 94
-#         $importe = 12100
-#         $moneda = "DOL"
-#         $ctz = 65
-#         $tipo_doc_rec = 80
-#         $nro_doc_rec = 20000000001
-#         $tipo_cod_aut = "E"
-#         $cod_aut = 70417054367476
-
-#         $url = $PyQR.GenerarImagen($ver, $fecha, $cuit, $pto_vta, $tipo_cmp, $nro_cmp,
-#                                    $importe, $moneda, $ctz, $tipo_doc_rec, $nro_doc_rec,
-#                                    $tipo_cod_aut, $cod_aut)
-#         $url | Should -Not -BeNullOrEmpty
-#         Test-Path $url | Should -Be $true
-#     }
-# }
-
 $PyQR = New-Object -ComObject PyQR
 
 Describe "PyQR Tests" {
