cbc72d37a4ed68575bac1793e4232f379861377c
Mariano Reingart <reingart@gmail.com>
2025-04-06 16:18:03 -0500

    RECE1: ajusto version

--- rece1.py
+++ rece1.py
@@ -15,7 +15,7 @@
 __author__ = "Mariano Reingart (reingart@gmail.com)"
 __copyright__ = "Copyright (C) 2010-2015 Mariano Reingart"
 __license__ = "GPL 3.0"
-__version__ = "1.39a"
+__version__ = "1.39b"
 
 import datetime
 import os
