f5127775ce7367e92af3a1e3fd724bb9a4d32363
Ian Gonzalez <ian@serfe.com>
2025-05-31 21:16:54 -0300

    feat: support activity codes

--- wsfev1.py
+++ wsfev1.py
@@ -134,7 +134,7 @@ class WSFEv1(BaseWS):
             cbt_desde=0, cbt_hasta=0, imp_total=0.00, imp_tot_conc=0.00, imp_neto=0.00,
             imp_iva=0.00, imp_trib=0.00, imp_op_ex=0.00, fecha_cbte="", fecha_venc_pago=None,
             fecha_serv_desde=None, fecha_serv_hasta=None, #--
-            moneda_id="PES", moneda_ctz="1.0000",  cond_iva_receptor=1, caea=None, fecha_hs_gen=None, **kwargs
+            moneda_id="PES", moneda_ctz="1.0000",  cond_iva_receptor=1, act_codigos=[], caea=None, fecha_hs_gen=None, **kwargs
             ):
 
         "Creo un objeto factura (interna)"
@@ -149,6 +149,7 @@ class WSFEv1(BaseWS):
                 'fecha_venc_pago': fecha_venc_pago,
                 'moneda_id': moneda_id, 'moneda_ctz': moneda_ctz,
                 'cond_iva_receptor': cond_iva_receptor,
+                'act_codigos': act_codigos,
                 'concepto': concepto, 'fecha_hs_gen': fecha_hs_gen,
                 'cbtes_asoc': [],
                 'tributos': [],
@@ -256,6 +257,11 @@ class WSFEv1(BaseWS):
                     'MonId': f['moneda_id'],
                     'MonCotiz': f['moneda_ctz'],
                     'CondicionIVAReceptorId': f['cond_iva_receptor'],
+                    'Actividades': f['act_codigos'] and [
+                        {'Actividad': {
+                            'Id': actividad_id,
+                        }}
+                        for actividad_id in f['act_codigos']] or None,
                     'CbtesAsoc': f['cbtes_asoc'] and [
                         {'CbteAsoc': {
                             'Tipo': cbte_asoc['tipo'],
@@ -395,6 +401,11 @@ class WSFEv1(BaseWS):
                     'MonId': f['moneda_id'],
                     'MonCotiz': float(f['moneda_ctz']),
                     'CondicionIVAReceptorId': f['cond_iva_receptor'],
+                    'Actividades': f['act_codigos'] and [
+                        {'Actividad': {
+                            'Id': actividad_id,
+                        }}
+                        for actividad_id in f['act_codigos']] or None,
                     'CbtesAsoc': [
                         {'CbteAsoc': {
                             'Tipo': cbte_asoc['tipo'],
@@ -459,6 +470,9 @@ class WSFEv1(BaseWS):
                     'moneda_id': resultget.get('MonId'),
                     'moneda_ctz': resultget.get('MonCotiz'),
                     'cond_iva_receptor': resultget.get('CondicionIVAReceptorId'),
+                    'act_codigos': [
+                        actividad['Actividad']['Id']
+                        for actividad in resultget.get('Actividades', [])],
                     'cbtes_asoc': [
                         {
                             'tipo': cbte_asoc['CbteAsoc']['Tipo'],
@@ -579,6 +593,11 @@ class WSFEv1(BaseWS):
                     'MonId': f['moneda_id'],
                     'MonCotiz': f['moneda_ctz'],
                     'CondicionIVAReceptorId': f['cond_iva_receptor'],
+                    'Actividades': f['act_codigos'] and [
+                        {'Actividad': {
+                            'Id': actividad_id,
+                        }}
+                        for actividad_id in f['act_codigos']] or None,
                     'CbtesAsoc': [
                         {'CbteAsoc': {
                             'Tipo': cbte_asoc['tipo'],
@@ -769,6 +788,11 @@ class WSFEv1(BaseWS):
                     'MonId': f['moneda_id'],
                     'MonCotiz': f['moneda_ctz'],
                     'CondicionIVAReceptorId': f['cond_iva_receptor'],
+                    'Actividades': f['act_codigos'] and [
+                        {'Actividad': {
+                            'Id': actividad_id,
+                        }}
+                        for actividad_id in f['act_codigos']] or None,
                     'CbtesAsoc': [
                         {'CbteAsoc': {
                             'Tipo': cbte_asoc['tipo'],
@@ -1128,6 +1152,9 @@ def main():
                 wsfev1.AgregarComprador(80, "30500010912", 99.99)
                 wsfev1.AgregarComprador(80, "30999032083", 0.01)
 
+            act_codes = [883101, 883102]
+            wsfev1.EstablecerCampoFactura("act_codigos", act_codes)
+
             # datos de Factura de Crédito Electrónica MiPyMEs (FCE):
             if '--fce' in sys.argv:
                 wsfev1.AgregarOpcional(2101, "2850590940090418135201")  # CBU
@@ -1229,6 +1256,7 @@ def main():
         p_assert_eq(wsfev1.ObtenerTagXml('CondicionIVAReceptorId'), "1")
         p_assert_eq(wsfev1.ObtenerTagXml('DocTipo'), "80")
         p_assert_eq(wsfev1.ObtenerTagXml('DocNro'), "30500010912")
+        p_assert_eq(wsfev1.ObtenerTagXml('Actividades', 'Actividad', 0, 'Id'), "883101")
 
     if "--parametros" in sys.argv:
         import codecs
