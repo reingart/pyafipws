9f29e7b92730ef5f7a39ea5e5afcb152559898bb
nicolaskug <nicolas.kugel@fierro.com.ar>
2026-01-29 10:10:00 -0300

    Actualizo versión future en archivo setup

--- setup.py
+++ setup.py
@@ -70,7 +70,7 @@ setup(
         "tabulate>=0.8.5",
         "certifi>=2020.4.5.1",
         "qrcode>=6.1",
-        "future>=0.18.2",
+        "future>=1.0.0",
     ],
     extras_require={
         "opt": ["pywin32==304;sys_platform == 'win32' and python_version > '3'"]
